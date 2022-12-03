048134a75d450171bc673c4bb0716712fb0169a4
Milena Emmert <milena.emmert@outlook.com>
2022-12-02 22:43:31 -0300

    Basis for React projects

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>Tome, cavalo 🐎</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+// export { NameComponent } from './name/name.component'
