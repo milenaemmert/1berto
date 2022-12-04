35396a8caf5b5f0728d70caf5daeb37c3807ee00
Milena Emmert <milena.emmert@outlook.com>
2022-12-04 15:40:17 -0300

    Feat: using styled components

---
+++ src/assets/base/variables.js
@@ -0,0 +1,13 @@
+export const colors = {
+  white: '#ffffff',
+  cinder: '#151422',
+  bg: 'linear-gradient(45deg, #b1afc8, #dcdcdc)'
+}
+
+export const paddings = {
+  large: '2em',
+  medium: '1.5em',
+  mobile: '1em'
+}
+
+
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,44 @@
 import ReactDOM from 'react-dom/client'
 import App from './app'
-import './assets/base/global.css'
+import { createGlobalStyle } from 'styled-components'
+import { colors, paddings } from './assets/base/variables'
+import './assets/base/fonts.css'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-size: 16px;
+    font-weight: normal;
+    border: none;
+  }
+
+  ::selection {
+    background-color: #dcdcdc;
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  body {
+    background-image: ${colors.bg};
+    overflow: hidden;
+    font-family: 'Shippori Mincho', serif;
+  }
+`
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-  <App />
+  <>
+    <GlobalStyle />
+    <App />
+  </>
 )
