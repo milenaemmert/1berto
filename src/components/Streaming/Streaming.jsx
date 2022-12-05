2bbf110b198c57b3a8012938f7074a3d848d2004
Milena Emmert <milena.emmert@outlook.com>
2022-12-04 23:06:08 -0300

    Add streaming icons and Streaming component

---
+++ src/components/Streaming/Streaming.jsx
@@ -0,0 +1,12 @@
+import { apple, deezer, google, spotify } from '../../assets/base/icons'
+
+export const Streaming = () => {
+  return (
+    <div>
+      <a href="#">{apple}</a>
+      <a href="#">{deezer}</a>
+      <a href="#">{google}</a>
+      <a href="#">{spotify}</a>
+    </div>
+  )
+}
